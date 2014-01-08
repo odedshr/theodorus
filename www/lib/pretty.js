3556fe83788d2db79fe5d4640b7ad12bb9624e22
Oded Sharon <odedshr@gmail.com>
2014-01-08 18:31:35 +0200

    fixed timezone issues

--- www/lib/pretty.js
+++ www/lib/pretty.js
@@ -7,35 +7,35 @@
 // Takes an ISO time and returns a string representing how
 // long ago the date represents.
 function prettyDate(time){
-	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
-		diff = (((new Date()).getTime() - date.getTime()) / 1000),
-		day_diff = Math.floor(diff / 86400);
-			
-	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 ) {
+    var date = new Date((time || "")),//.replace(/-/g,"/").replace(/[TZ]/g," ") => this appeared in the original code, it reset timezones...
+        diff = (((new Date()).getTime() - date.getTime()) / 1000),
+        day_diff = Math.floor(diff / 86400);
+
+    if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 ) {
         return time;
     }
 
-	return day_diff == 0 && (
-			diff < 60 && "just-now" ||
-			diff < 120 && "a-minute-ago" ||
+    return day_diff == 0 && (
+            diff < 60 && "just-now" ||
+            diff < 120 && "a-minute-ago" ||
             diff < 240 && "two-minutes-ago" ||
-			diff < 900 && Math.floor( diff / 60 ) + "-minutes-ago" ||
+            diff < 900 && Math.floor( diff / 60 ) + "-minutes-ago" ||
             diff < 1800 && "quarter-of-an-hour-ago" ||
             diff < 2400 && "half-of-an-hour-ago" ||
             diff < 3600 && Math.floor( diff / 60 ) + "-minutes-ago" ||
             diff < 7200 && "an-hour-ago" ||
             diff < 1440 && "two-hours-ago" ||
-			diff < 86400 && Math.floor( diff / 3600 ) + "-hours-ago") ||
-		day_diff == 1 && "yesterday" ||
+            diff < 86400 && Math.floor( diff / 3600 ) + "-hours-ago") ||
+        day_diff == 1 && "yesterday" ||
         day_diff == 2 && "two-days-ago" ||
-		day_diff < 7 && day_diff + "-days-ago" ||
+        day_diff < 7 && day_diff + "-days-ago" ||
         day_diff < 14 && "a-week-ago" ||
         day_diff < 21 && "two-weeks-ago" ||
-		day_diff < 31 && Math.ceil( day_diff / 7 ) + "-weeks-ago";
+        day_diff < 31 && Math.ceil( day_diff / 7 ) + "-weeks-ago";
 }
 
 /*
-    @return string based on the pattern dd/mm/yyyy, hh:mm
+ @return string based on the pattern dd/mm/yyyy, hh:mm
  */
 function normalizeDate(dateString) {
     var d = new Date(dateString);
@@ -44,13 +44,13 @@ function normalizeDate(dateString) {
 
 // If jQuery is included in the page, adds a jQuery plugin to handle it as well
 if ( typeof jQuery != "undefined" ) {
-	jQuery.fn.prettyDate = function(){
-		return this.each(function(){
-			var date = prettyDate(this.title);
-			if ( date )
-				jQuery(this).text( date );
-		});
-	};
+    jQuery.fn.prettyDate = function(){
+        return this.each(function(){
+            var date = prettyDate(this.title);
+            if ( date )
+                jQuery(this).text( date );
+        });
+    };
 } else if (typeof exports !== "undefined") {
     exports.prettyDate = prettyDate.bind(prettyDate);
     exports.normalizeDate = normalizeDate.bind(normalizeDate);
