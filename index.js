67d4d2a5b3d8c62b9b7b1f9b35dba123ed012f31
Oded Sharon <odedsh@gmail.com>
2016-05-01 10:41:16 +0100

    renamed back to index.js (so open shift could find it) and added node mailer that got lost

--- index.js
+++ index.js
@@ -5,12 +5,11 @@
   var bodyParser = require('body-parser');
   var app = express();
 
-  var config = require('../helpers/config.js');
-  var Errors = require('../helpers/Errors.js');
-  var log = require('../helpers/logger.js');
-  var db = require('../helpers/db.js');
-
-  var populate = require('../helpers/RouteManager.js');
+  var config = require('./helpers/config.js');
+  var db = require('./helpers/db.js');
+  var Errors = require('./helpers/Errors.js');
+  var log = require('./helpers/logger.js');
+  var populate = require('./helpers/RouteManager.js');
 
   function setHeaders(req, res, next) {
     if (req.headers.origin !== undefined) {
